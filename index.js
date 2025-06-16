d320fdb78d4ae5cbfd0391ada0b817734b3336ae
unknown <lela.office123@gmail.com>
2025-06-16 15:40:29 +0700

    close modal outside the form

--- index.js
+++ index.js
@@ -9,9 +9,16 @@ function openModal(){
     modal.style.display = 'grid';
 }
 
-// CLOSE MODAL
+// CLOSE MODAL INSIDE THE FORM BY BUTTON "X"
 closeBtn.addEventListener('click', closeModal);
 
 function closeModal(){
     modal.style.display = 'none';
 }
+
+// CLOSE MODAL ANYWHERE OUTSIDE THE FORM
+modal.addEventListener('click', function(e){
+    if(e.target.classList.contains('modal')){
+        closeModal();
+    }
+})
